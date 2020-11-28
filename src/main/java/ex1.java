6a77884e5229d8a0cf3c453c33e3ea203e61aa35
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-11-28 14:38:31 +0200

    add ex1

---
+++ src/main/java/ex1.java
@@ -0,0 +1,11 @@
+public class ex1 {
+    public static void main(String[] args) {
+
+        System.out.println(add(-14, -16));
+    }
+
+    public static int add(int x, int y) {
+
+        return x + y;
+    }
+}
